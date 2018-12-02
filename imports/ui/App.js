ee05acaaf37f5a43efd632308ea17ba5553c3b53
shabbir3s <shabbir@salahsoftwaresolution.com>
2018-12-02 20:16:40 +0600

    Add hide n show

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,6 +6,13 @@ import Task from './Task';
 
 class App extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            hideCompleted: false
+        };
+    }
+
     handleSubmit(e){
         e.preventDefault();
 
@@ -21,8 +28,18 @@ class App extends Component{
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted(){
+        this.setState({
+            hideCompleted: !this.state.hideCompleted
+        });
+    }
+
     renderTasks(){
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if(this.state.hideCompleted){
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -31,7 +48,17 @@ class App extends Component{
         return(
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+
+                    <label className="hide-completed">
+                        <input
+                        type="checkbox"
+                        readOnly
+                        checked={this.state.hideCompleted}
+                        onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Task
+                    </label>
 
                     <form 
                     className="new-task" 
@@ -61,5 +88,6 @@ export default withTracker(() => {
                     createdAt: -1
                 }
             }).fetch(),
+        incompleteCount: Tasks.find({checked: {$ne: true}}).count()
     };
 })(App);

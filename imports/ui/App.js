8f69201746dec5cfd29bc23857b0bbb57e08e48e
shabbir3s <shabbir@salahsoftwaresolution.com>
2018-12-02 17:09:24 +0600

    Add collection & dynamic lists

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tasks } from '../api/tasks';
 import Task from './Task';
 
-export default class App extends Component{
-    getTasks(){
-        return [
-            {_id: 1, text: 'This is task 1'},
-            {_id: 2, text: 'This is task 2'},
-            {_id: 3, text: 'This is task 3'},
-            {_id: 4, text: 'This is task 4'},
-            {_id: 5, text: 'This is task 5'}
-        ];
+class App extends Component{
+
+    handleSubmit(e){
+        e.preventDefault();
+
+        //Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(), //current time
+        });
+
+        //Clear the form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
     renderTasks(){
-        return this.getTasks().map((task) => (
+        return this.props.tasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -23,6 +32,17 @@ export default class App extends Component{
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+
+                    <form 
+                    className="new-task" 
+                    onSubmit={this.handleSubmit.bind(this)} >
+                        <input
+                        type="text"
+                        ref="textInput"
+                        placeholder="Type to add new tasks"
+                        />
+                    
+                    </form>
                 </header>
 
                 <ul>
@@ -32,3 +52,14 @@ export default class App extends Component{
         );
     }
 }
+
+export default withTracker(() => {
+    return {
+        tasks: Tasks.find({}, 
+            {sort: 
+                {
+                    createdAt: -1
+                }
+            }).fetch(),
+    };
+})(App);
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { Tasks } from '../api/tasks';
+import classnames from 'classnames';
 
 export default class Task extends Component{
+    toggleChecked(){
+        //set the checked property to the opposite of its current value
+        Tasks.update(this.props.task._id, {
+            $set: {
+                checked: !this.props.task.checked
+            },
+        });
+    }
+
+    deleteThisTask(){
+        Tasks.remove(this.props.task._id);
+    }
+
     render(){
+
+        const taskClassName = classnames({
+            checked: this.props.task.checked
+        });
+
         return(
-            <li>{this.props.task.text}</li>
+            <li className={taskClassName}>
+                <button 
+                className="delete" 
+                onClick={this.deleteThisTask.bind(this)} >
+                    &times;
+                </button>
+
+                <input 
+                type="checkbox" 
+                readOnly
+                checked={!!this.props.task.checked}
+                onClick={this.toggleChecked.bind(this)}
+                />
+                <span className="text">{this.props.task.text}</span>
+            </li>
         );
     }
 }
